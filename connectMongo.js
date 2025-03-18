a5aef3d18e5052ecabe5269858df6fbe86ffa49d
VR1206 <143418316+VR1206@users.noreply.github.com>
2025-03-18 09:20:18 +0530

    Update connectMongo.js

--- connectMongo.js
+++ connectMongo.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose')
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
-        console.log("Connect to MongoDB successfully")
-    } catch (error) {
-        console.log("Connect failed " + error.message )
-    }
-}
-
-module.exports = connectDB
+MONGO_URL = "mongodb+srv://testing:Jakhar9014@vip.qrk6v.mongodb.net/?retryWrites=true&w=majority&appName=VIP"
